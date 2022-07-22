5ff6f717206675ae530a06dd8cba26f67c21be06
John Thomas <thomas07@vt.edu>
2022-07-22 14:48:40 -0400

    add last_trade_date_or_contract_month for futures

--- lib/models/ib/future.rb
+++ lib/models/ib/future.rb
@@ -1,15 +1,20 @@
 #require 'models/ib/contract'
 module IB
-  class Future  < Contract
+  class Future  < ::IB::Contract
     validates_format_of :sec_type, :with => /\Afuture\z/,
       :message => "should be a Future"
+
     def default_attributes
       super.merge :sec_type => :future, currency:'USD'
     end
+
     def to_human
       "<Future: " + [symbol, expiry, currency].join(" ") + ">"
     end
 
+    def last_trade_date_or_contract_month
+      self[:expiry]
+    end
   end
-  end
+end
 
